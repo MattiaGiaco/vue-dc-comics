de9dc03249adc6363d7c220103258140d251a2c3
mattiagiaco <martex781227@gmail.com>
2021-11-25 12:54:37 +0100

    almost complete

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,12 +5,19 @@
     </div>
 
     <div class="current-series">
-      <div class="container">
+      <div class="title">
+        current series
+      </div>
+      <div class="container box">
         <SeriesCard 
         v-for="(product, index) in thumbData"  
         :key="index"
         :product="product"
         />
+      </div>
+
+      <div class="button">
+        <button>load more</button>
       </div>
     </div>
   </main>
@@ -35,14 +42,51 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+@import "../assets/scss/var.scss";
+
   main .junbotron{
     width: 100%;
     height: 400px;
     background-image: url(../assets/img/jumbotron.jpg);
+    background-size: cover;
   }
 
   main .current-series{
+    position: relative;
     min-height: 300px;
     background-color: #1c1c1c;
+    .box{
+      display: flex;
+      flex-wrap: wrap;
+      padding-bottom: 80px;
+    }   
   }
-</style>
+  .button{
+    display: flex;
+    justify-content: center;
+    padding-bottom: 20px;
+    button{
+      padding: 10px 50px;
+      background-color: $primaryColor;
+      border: none;
+      color: white;
+      text-transform: uppercase;
+      font-weight: bold;
+    }
+  }
+
+  .title{
+    position: absolute;
+    top: -30px;
+    left: 20%;
+    background-color: $primaryColor;
+    width: 18%;
+    text-align: center;
+    padding: 20px;
+    color: white;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 25px;
+  }
+      </style>
--- src/components/SeriesCard.vue
+++ src/components/SeriesCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
-    <img :src="product.thumb" alt="">
-    <h3></h3>
+    <img :src="product.thumb" :alt="product.series">
+    <h3>{{product.series}}</h3>
   </div>
 </template>
 
@@ -15,8 +15,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
   .card{
-    width: 200px;
-    height: 200px;
+    width: calc(100% / 6);
+    height: 250px;
+    margin: 30px 0;
+    border: 20px solid #1c1c1c;
+    img{
+      width: 100%;
+      height: 100%;
+      margin-bottom: 10px;
+      object-fit: cover;
+    }
   }
 </style>
